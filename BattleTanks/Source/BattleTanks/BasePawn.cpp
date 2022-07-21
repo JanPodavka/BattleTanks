6b666109c1b244e0495f4efb4f7bb18d9e027eb8
Jan Podávka <72079041+JanPodavka@users.noreply.github.com>
2022-07-22 00:59:33 +0200

    Create skeleton for our BasePawn (different types of Components)

--- BattleTanks/Source/BattleTanks/BasePawn.cpp
+++ BattleTanks/Source/BattleTanks/BasePawn.cpp
@@ -2,13 +2,24 @@
 
 
 #include "BasePawn.h"
+#include "Components/CapsuleComponent.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	//Root - Capsule
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+	RootComponent = CapsuleComp; //Set Capsule as Rooot Component
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh")); // Create Static Mesh 
+	BaseMesh->SetupAttachment(CapsuleComp); // Atteched to Root component
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint ->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
